89558fdd76edd2565f9604a68010737e60e77a8c
paperrin <paperrin@student.42.fr>
2017-09-30 02:31:29 +0200

    Adding fract_tree

--- src/put_pixel.c
+++ src/put_pixel.c
@@ -6,7 +6,7 @@
 /*   By: paperrin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/30 14:39:45 by paperrin          #+#    #+#             */
-/*   Updated: 2017/09/13 10:41:43 by paperrin         ###   ########.fr       */
+/*   Updated: 2017/09/30 00:04:06 by paperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,17 @@ unsigned int			get_color(void *mlx_core, t_color_rgb color)
 	return (mlx_get_color_value(mlx_core, encoded));
 }
 
-void					put_pixel(const t_app *app
-		, t_vec2i pos, t_color_rgb rgb)
+void					put_pixel(t_vec3f pos, t_color_rgb rgb, void *app)
 {
 	size_t			index;
 	unsigned int	color_encoded;
 	int				i;
 
-	color_encoded = get_color(app->mlx.core, rgb);
-	index = ((pos.y * app->draw_buf.bytes_width) + (pos.x * 4));
+	color_encoded = get_color(((t_app*)app)->mlx.core, rgb);
+	index = (((int)pos.y * ((t_app*)app)->draw_buf.bytes_width)
+		+ ((int)pos.x * 4));
 	i = -1;
 	while (++i < 3)
-		app->draw_buf.pixels[index + i] = color_encoded >> (i * 8);
+		((t_app*)app)->draw_buf.pixels[index + i] = color_encoded
+			>> (i * 8);
 }
